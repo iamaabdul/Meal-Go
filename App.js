08739962735fe708045f3893517153e7af451949
iamaabdul <me_abdulrehman@outlook.com>
2022-02-01 15:57:08 +0500

    Eslint

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StatusBar, StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <ExpoStatusBar style="auto" />
+    </SafeAreaView>
   );
 }
 
@@ -16,5 +16,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    marginTop: StatusBar.currentHeight,
   },
 });
